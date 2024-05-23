7a6f8da27e7be784cd8a7e7533f6d2ebcb23f1d0
J Nick Baughan <JNickBaughan@gmail.com>
2024-05-23 09:02:15 -0400

    add indicator arrows

--- src/client/components/Grid.tsx
+++ src/client/components/Grid.tsx
@@ -1,8 +1,39 @@
 import React from "react";
 
-import {  PatientDetails  } from "../../common/types/types";
+import {  PatientDetails, PatientStats  } from "../../common/types/types";
 
 export const Grid = ({ patient }: { patient: PatientDetails | null }) => {
+
+    var getArrow = (inx: number, curr: number, prev?: number) => {
+        
+        if(inx === 0) { 
+            return null;
+        }
+        
+        if(!prev || curr === prev) { 
+            return "-";
+        }
+        
+        if(curr > prev!) {
+            return (<i className="arrow up"></i>);
+        }
+
+        return (<i className="arrow down"></i>);
+    }
+
+    var buildRow = (s: PatientStats, inx: number, arr: PatientStats[]) => {
+        const prevRow = inx !== 0 ? arr[inx - 1] : null;
+        return (
+            <div className={`flex-grid${inx !== 0 ? " border-top" : "" }`}>
+                <div className="col">{s.date}</div>
+                <div className="col">{s.heartRate} {getArrow(inx, s.heartRate, prevRow?.heartRate)}</div>
+                <div className="col">{s.diastolic} {getArrow(inx, s.diastolic, prevRow?.diastolic)}</div>
+                <div className="col">{s.systolic} {getArrow(inx, s.systolic, prevRow?.systolic)}</div>
+            </div>
+        )
+    }
+
+
     return (
         <>
             <h1>Patient Stats</h1>
@@ -13,14 +44,7 @@ export const Grid = ({ patient }: { patient: PatientDetails | null }) => {
                 <div className="col">Systolic</div>
             </div>
             {
-                patient?.stats?.map((s, inx) => (
-                    <div className={`flex-grid${inx !== 0 ? " border-top" : "" }`}>
-                        <div className="col">{s.date}</div>
-                        <div className="col flex-grid-thirds">{s.heartRate}</div>
-                        <div className="col">{s.diastolic}</div>
-                        <div className="col">{s.systolic}</div>
-                    </div>
-                ))
+                patient?.stats?.sort((a, b) => new Date(a.date) - new Date(b.date))?.map(buildRow)
             }
         </>)
 }
--- src/server/routes/patients.ts
+++ src/server/routes/patients.ts
@@ -15,13 +15,18 @@ router.get('/:id', (req: Request, res: Response<PatientDetails>)  => {
     const { id } = req.params;
     //TODO: make mock database and lookup real contact 
     res.send({ id: 15
-        ,firstName: "Nick!"
+        ,firstName: "Nick"
         ,lastName: "Baughan"
         ,photoUrl: ""
         ,stats: [
-             { date: "2024-01-01", heartRate: 72, diastolic: 80, systolic: 124 }
-            ,{ date: "2024-02-11", heartRate: 72, diastolic: 78, systolic: 119 }
-            ,{ date: "2024-04-07", heartRate: 74, diastolic: 82, systolic: 118 }
+             { date: "2023-09-03", heartRate: 74, diastolic: 82, systolic: 117 }
+            ,{ date: "2023-11-11", heartRate: 78, diastolic: 82, systolic: 128 }
+            ,{ date: "2024-02-11", heartRate: 73, diastolic: 78, systolic: 119 }
+            ,{ date: "2023-12-03", heartRate: 85, diastolic: 82, systolic: 118 }
+            ,{ date: "2024-01-01", heartRate: 79, diastolic: 80, systolic: 125 }
+            ,{ date: "2024-04-07", heartRate: 71, diastolic: 82, systolic: 121 }
+            ,{ date: "2024-04-07", heartRate: 74, diastolic: 81, systolic: 122 }
+            ,{ date: "2022-09-10", heartRate: 80, diastolic: 78, systolic: 125 }
         ] 
     });
 });
