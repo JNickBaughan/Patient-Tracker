1c933af17b1eafc54c5c2bf8efc302b6028dd131
J Nick Baughan <JNickBaughan@gmail.com>
2024-05-27 09:10:19 -0400

    refactor row to separate file

--- src/client/components/Grid.tsx
+++ src/client/components/Grid.tsx
@@ -2,56 +2,14 @@ import React from "react";
 
 import {  PatientDetails, PatientStats  } from "../../common/types/types";
 
+import { Row } from "./Row";
+
 export const Grid = ({ patient }: { patient: PatientDetails | null }) => {
 
-    var getArrow = (inx: number, curr: number, prev?: number) => {
-        
-        if(inx === 0) { 
-            return null;
-        }
-        
-        if(!prev || curr === prev) { 
-            return "-";
-        }
-        
-        if(curr > prev!) {
-            return (<i className="arrow up"></i>);
-        }
-
-        return (<i className="arrow down"></i>);
+    var buildRow = (stats: PatientStats, index: number, arr: PatientStats[]) => {
+        const prevStats = index !== 0 ? arr[index - 1] : null;
+        return <Row stats={stats} prevStats={prevStats} index={index} />
     }
-
-    var heartRateRanges = [60, 73, 90];
-    var getCssClass = (heartRate: number, ranges: number[]) => {
-        if(heartRate <= ranges[0])
-        {
-            return " great"
-        }
-
-        if(heartRate >= ranges[1] && heartRate < ranges[2])
-        {
-            return " good"
-        }
-
-        if(heartRate >= ranges[2])
-        {
-            return " danger"
-        }
-        return ""
-    }
-
-    var buildRow = (s: PatientStats, inx: number, arr: PatientStats[]) => {
-        const prevRow = inx !== 0 ? arr[inx - 1] : null;
-        return (
-            <div className={`flex-grid${inx !== 0 ? " border-top" : "" }`}>
-                <div className="col">{new Date(s.date).toLocaleDateString()}</div>
-                <div className={`col${getCssClass(s.heartRate, heartRateRanges)}`}>{s.heartRate} {getArrow(inx, s.heartRate, prevRow?.heartRate)}</div>
-                <div className="col">{s.diastolic} {getArrow(inx, s.diastolic, prevRow?.diastolic)}</div>
-                <div className="col">{s.systolic} {getArrow(inx, s.systolic, prevRow?.systolic)}</div>
-            </div>
-        )
-    }
-
 
     return (
         <>
